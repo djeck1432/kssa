2ad17ecba86d835988bab4250a424a549b5758bd
Oleh12345 <oleh.kostashchuk@gmail.com>
2016-08-25 09:10:59 +0200

    Add existing file

--- project/project/settings.py
+++ project/project/settings.py
@@ -120,12 +120,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = "app-root/repo/wsgi/static"
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATICFILES_DIRS = (
+    ('assets', 'app-root/repo/wsgi/openshift/static'),
+
+    )
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
